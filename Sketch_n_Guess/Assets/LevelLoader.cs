6381ca717dfed1a13cdbcac341682b742213b762
Luka Capan <luka.capan@gmail.com>
2024-04-06 17:49:33 +0200

    Izbrisana dodatna skripta za učitavanje levela. Sada jedna skripta učitava sljedeći level i tranziciju.

--- Sketch_n_Guess/Assets/LevelLoader.cs
+++ Sketch_n_Guess/Assets/LevelLoader.cs
@@ -1,25 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
     public Animator transition;
-    public float transitionTime = 1f;
+    public float transitionTime = 0.45f;
 
-    // Update is called once per frame
-    void Update() {
-        if(Input.GetMouseButtonDown(0)) {
-            LoadNextLevel();
-        }
+    public void LoadNextLevel(string loadScene) {
+        //StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+        StartCoroutine(LoadLevel(loadScene));
     }
 
-    public void LoadNextLevel() {
-        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
-    }
-
-    IEnumerator LoadLevel(int levelIndex) {
+    IEnumerator LoadLevel(string sceneName) { // int sceneIndex
         // Play animation
         transition.SetTrigger("StartTrans");
 
@@ -27,6 +22,11 @@ public class LevelLoader : MonoBehaviour
         yield return new WaitForSeconds(transitionTime);
 
         // Load scene
-        SceneManager.LoadScene(levelIndex);
+        if(sceneName == "Quit") {
+            Application.Quit();
+            Debug.Log("You have exited the game!");
+        } else { 
+            SceneManager.LoadScene(sceneName);
+        } 
     }
 }

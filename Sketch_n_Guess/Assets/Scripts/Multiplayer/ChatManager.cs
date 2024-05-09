e284ea25dc1488e68bc6a14adae49ab52669793a
Luka Capan <luka.capan@gmail.com>
2024-05-09 20:51:42 +0200

    Omogućen unos teksta igrača i vidljiv svima. Closes #86

--- Sketch_n_Guess/Assets/Scripts/Multiplayer/ChatManager.cs
+++ Sketch_n_Guess/Assets/Scripts/Multiplayer/ChatManager.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
 
 public class ChatManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public InputField inputMessage;
+    public GameObject UserMessages;
+    public GameObject Content;
+
+    private void Start() {
+        // Add event listener for the end of edit (when user finishes editing)
+        inputMessage.onEndEdit.AddListener(delegate { SendMessageOnEnter(); });
+    }
+    
+    public void SendMessageOnEnter() {
+        // Check if the input field is not empty and if the user pressed the "Return" key
+        if(!string.IsNullOrEmpty(inputMessage.text) && Input.GetKeyDown(KeyCode.Return)) {
+            SendMessage();
+        }
+    }
+    
+    public void SendMessage() {
+        GetComponent<PhotonView>().RPC("GetMessage", RpcTarget.All, (PhotonNetwork.NickName + ": " + inputMessage.text)); 
+
+        inputMessage.text = "";
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    [PunRPC]
+    public void GetMessage(string ReceiveMessage) {
+        GameObject M = Instantiate(UserMessages, Vector3.zero, Quaternion.identity, Content.transform);
+        M.GetComponent<UserMessages>().myMessage.text = ReceiveMessage;
     }
 }

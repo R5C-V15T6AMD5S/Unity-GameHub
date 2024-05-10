555b4587bc4275b97071aa0bbccf01663f6620da
Luka Capan <luka.capan@gmail.com>
2024-05-10 19:50:37 +0200

    Manje optimizacijske promjene i bolje skaliranje elemenata.

--- Sketch_n_Guess/Assets/Scripts/Singleplayer/LineGenerator.cs
+++ Sketch_n_Guess/Assets/Scripts/Singleplayer/LineGenerator.cs
@@ -10,6 +10,14 @@ public class LineGenerator : MonoBehaviour
     // referenca na aktivnu liniju
     Line activeLine;
 
+    // referenca na aktivni canvas
+    RectTransform canvasRect;
+
+    void Start() {
+        // uzima RectTransform komponentu od canvasa
+        canvasRect = GetComponent<RectTransform>();
+    }
+
     void Update() {
         // kada kliknemo lijevi klik misem crtanje
         if (Input.GetMouseButtonDown(0)) {
@@ -25,8 +33,18 @@ public class LineGenerator : MonoBehaviour
         // ako activeLine nije null želimo dobiti poziciju miša koju proslijeđujemo updatePosition funkciji
         if(activeLine != null) {
             Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //Vector2 mousePos = GetClampedMousePosition();
             activeLine.updatePosition(mousePos);
         }
     }
-}
 
+    // funkcija za limitiranje linije u samom canvasu
+    /*Vector2 GetClampedMousePosition() {
+        Vector2 mousePos = Input.mousePosition;
+        Vector2 clampedMousePos = new Vector2(
+            Mathf.Clamp(mousePos.x, canvasRect.position.x - canvasRect.sizeDelta.x / 2f, canvasRect.position.x + canvasRect.sizeDelta.x / 2f),
+            Mathf.Clamp(mousePos.y, canvasRect.position.y - canvasRect.sizeDelta.y / 2f, canvasRect.position.y + canvasRect.sizeDelta.y / 2f)
+        );
+        return clampedMousePos;
+    }*/
+}
